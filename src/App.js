68653376f1b9f4e05bcedb692b0695325f25d027
Shinya Hara <s.hara@sonicmoov.com>
2022-03-10 08:48:27 +0900

    feat: Add authenticator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, Button } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello from V2</h1>
-        </header>
-      </div>
-  );
+function App({ signOut }) {
+    return (
+        <div className="App">
+            <header>
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>We now have Auth!</h1>
+            </header>
+            <Button onClick={signOut}>Sign out</Button>
+        </div>
+    );
 }
 
-export default App;
+export default withAuthenticator(App);
